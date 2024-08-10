9cb619515f1dc3b1a66af2c2e17379d366d8c328
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-08-11 00:45:27 +0530

    User controller completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
+import { v2 as cloudinary } from "cloudinary";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
@@ -23,4 +23,10 @@ app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
+});
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
