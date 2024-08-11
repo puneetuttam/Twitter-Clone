7d97c49e21dfcdb79fea15bf97fcbeb11918e38f
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-08-11 20:01:51 +0530

    Post routes and controller added

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost,deletePost,commentOnPost,likeUnlikePost ,getAllPost,getLikedPosts} from "../controllers/post.controller.js";
+const router=express.Router();
+
+router.get('/all',protectRoute,getAllPost);
+router.get('/likes/:id',protectRoute,getLikedPosts  );
+router.post('/create',protectRoute,createPost);
+router.post('/like/:id',protectRoute,likeUnlikePost)
+router.post('/comment/:id',protectRoute,commentOnPost)
+router.delete('/:id',protectRoute,deletePost);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
-
+import postRoutes from './routes/post.routes.js'
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,16 +23,12 @@ console.log(process.env.MONGO_URI);
 app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
-});
-
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
